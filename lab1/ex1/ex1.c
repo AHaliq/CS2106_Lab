7e11c099ac4fdf1a41871d90fe2b91a55b987f66
AHaliq <liqo12@hotmail.com>
2019-08-24 16:47:20 +0800

    completed ex1

--- lab1/ex1/ex1.c
+++ lab1/ex1/ex1.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 // stdlib includes malloc() and free()
 
-// define instructions 
+// define instructions
 #define PREVIOUS -1
 #define NEXT 1
 #define DELETE 0
-
 
 // Node declaration ( Do not edit )
 typedef struct NODE
 {
 	int data;
-	struct NODE* previousNode;
-	struct NODE* nextNode;
+	struct NODE *previousNode;
+	struct NODE *nextNode;
 } node;
 
 // Function prototypes
-void insertNodeNext(int,int,node*);//TODO
-void insertNodePrevious(int,int,node*);// TODO
-void deleteNode(int,node*); // TODO
-void deleteList(node*); // TODO
+void insertNodeNext(int, int, node *);
+void insertNodePrevious(int, int, node *);
+void deleteNode(int, node *);
+void deleteList(node *);
 
-void printList(node*); // Implemented for you
+node *getNode(int, node *);
+
+void printList(node *); // Implemented for you
 
 // Start of main
 int main()
@@ -30,100 +31,134 @@ int main()
 	int position;
 	int instruction;
 	int value;
-	
+
 	// Declaration of the origin Node ( do not edit )
-	node* originNode = (node*)malloc(sizeof(node));
+	node *originNode = (node *)malloc(sizeof(node));
 	originNode->previousNode = originNode;
 	originNode->nextNode = originNode;
 	originNode->data = 0;
-	
-	// input and processing has been done for you. 
-	
-	while(scanf("%i%i",&instruction,&position) == 2)
-		//scanf() returns an int that corresponds to the number of values scanned. 
+
+	// input and processing has been done for you.
+
+	while (scanf("%i%i", &instruction, &position) == 2)
+	//scanf() returns an int that corresponds to the number of values scanned.
 	{
-		if(instruction == DELETE)
+		if (instruction == DELETE)
 		{
-			deleteNode(position,originNode);
+			deleteNode(position, originNode);
 		}
-		else if(instruction == NEXT)
+		else if (instruction == NEXT)
 		{
-			scanf("%i",&value);
-			insertNodeNext(position,value,originNode);
+			scanf("%i", &value);
+			insertNodeNext(position, value, originNode);
 		}
-		else if(instruction == PREVIOUS)
+		else if (instruction == PREVIOUS)
 		{
-			scanf("%i",&value);
-			insertNodePrevious(position,value,originNode);
+			scanf("%i", &value);
+			insertNodePrevious(position, value, originNode);
 		}
 	}
 	printList(originNode);
 	deleteList(originNode);
-	
 
 	printf("Circular List after delete\n");
 	printList(originNode);
-	
+
 	free(originNode);
-	
+
 	return 0;
 }
 
+//Function Implementations
 
-//Function Implementations 
-
-void insertNodePrevious(int position,int value, node* originNode)
+void insertNodePrevious(int position, int value, node *originNode)
 {
-	// TODO
+	node *cur = getNode(position, originNode);
+	node *pre = cur->previousNode;
+	node *new = (node *)malloc(sizeof(node));
+	new->data = value;
+	cur->previousNode = new;
+	pre->nextNode = new;
+	new->previousNode = pre;
+	new->nextNode = cur;
 }
 
-void insertNodeNext(int position, int value, node* originNode)
+void insertNodeNext(int position, int value, node *originNode)
 {
-	// TODO
+	node *cur = getNode(position, originNode);
+	node *nxt = cur->nextNode;
+	node *new = (node *)malloc(sizeof(node));
+	new->data = value;
+	cur->nextNode = new;
+	nxt->previousNode = new;
+	new->nextNode = nxt;
+	new->previousNode = cur;
 }
 
-void deleteNode (int position,node* originNode)
+void deleteNode(int position, node *originNode)
 {
-	// TODO
+	node *cur = getNode(position, originNode);
+	node *pre = cur->previousNode;
+	node *nxt = cur->nextNode;
+	pre->nextNode = nxt;
+	nxt->previousNode = pre;
+	cur->nextNode = NULL;
+	cur->previousNode = NULL;
+	free(cur);
 }
 
-void deleteList(node* originNode)
+void deleteList(node *originNode)
 {
-	// TODO
+	while (originNode->nextNode != originNode)
+	{
+		deleteNode(1, originNode);
+	}
+}
+
+// utils
+
+node *getNode(int position, node *originNode)
+{
+	int i = 0;
+	node *n = originNode;
+	while (i++ < position)
+	{
+		n = n->nextNode;
+	}
+	return n;
 }
 
 // printList has been implemented for you.
-// You may study the functions below to help you implement your own functions. 
-void printList(node* originNode)
+// You may study the functions below to help you implement your own functions.
+void printList(node *originNode)
 {
 	int count = 0;
 
 	printf("Printing clockwise:\n");
-	node* iterator = originNode->nextNode;
-	printf("[Pos %i:%i]",0,originNode->data);
+	node *iterator = originNode->nextNode;
+	printf("[Pos %i:%i]", 0, originNode->data);
 	printf("\n   |\n   v\n");
-	while(iterator != originNode)
+	while (iterator != originNode)
 	{
 		count++;
-		printf("[Pos %i:%i]",count,iterator->data);
+		printf("[Pos %i:%i]", count, iterator->data);
 		printf("\n   |\n   v\n");
 		iterator = iterator->nextNode;
 	}
-	printf("[Pos %i:%i]",0,originNode->data);	
-	
+	printf("[Pos %i:%i]", 0, originNode->data);
+
 	printf("\nPrinting counter-clockwise:\n");
 	iterator = originNode->previousNode;
-	printf("[Pos %i:%i]",0,originNode->data);
+	printf("[Pos %i:%i]", 0, originNode->data);
 	printf("\n   |\n   v\n");
-	while(iterator!= originNode)
+	while (iterator != originNode)
 	{
-		printf("[Pos %i:%i]",count,iterator->data);
+		printf("[Pos %i:%i]", count, iterator->data);
 		printf("\n   |\n   v\n");
 		iterator = iterator->previousNode;
 		count--;
 	}
-	printf("[Pos %i:%i]\n",0,originNode->data);		
+	printf("[Pos %i:%i]\n", 0, originNode->data);
 
 	return;
 }
-
