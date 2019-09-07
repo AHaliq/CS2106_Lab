8587908823e591eb66f50e9ab736b8af2b71eaa8
AHaliq <liqo12@hotmail.com>
2019-09-07 15:50:28 +0800

    completed lab2 ex2

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -8,6 +8,9 @@
  lab machine (Linux on x86)
  *************************************/
 
+#define TOKEN_LEN 19
+#define TOKEN_CNT 10 
+
 #include <stdio.h>
 #include <fcntl.h>      //For stat()
 #include <sys/types.h>
@@ -16,19 +19,72 @@
 #include <unistd.h>     //for fork(), wait()
 #include <string.h>     //for string comparison etc
 #include <stdlib.h>     //for malloc()
+#include <string.h>     //for strcmp()
+#include <stdarg.h>
 
 char** readTokens(int maxTokenNum, int maxTokenSize, int* readTokenNum, char* buffer);
 void freeTokenArray(char** strArr, int size);
 
+void print(char* str, ...);
+void printError(char * str, ...);
+char** readT(int* tokenCount);
+int cfileexists(const char* filename);
+
+const int INPUT_LENGTH = TOKEN_LEN + TOKEN_CNT * (1 + TOKEN_LEN) + 1;
+
 int main() {
-    //TODO add your code
-    
+    print("Hello, welcome to Genie!\n");
+    int tCount, childPid, run = 1;
+    char** tokens;
+    while(run) {
+        tokens = readT(&tCount);
+
+        if(strcmp(tokens[0], "quit") == 0) {
+            run = 0;
+        }else {
+            char exists = 0;
+            char fullPath[TOKEN_LEN + 5];
+            sprintf(fullPath, "%s", tokens[0]);
+
+            if(cfileexists(tokens[0])) {
+                exists = 1;
+            }else {
+                sprintf(fullPath, "/bin/%s", tokens[0]);
+                if(cfileexists(fullPath)) {
+                    exists = 1;
+                }else {
+                    printError("%s not found\n", tokens[0]);
+                }
+            }
+            // determine full path to command
+
+            if(exists) {
+                childPid = fork();
+                if(childPid == 0) {
+                    execv(fullPath, tokens);
+                }else {
+                    wait(NULL);
+                }
+            }
+            // run child command
+        }
+
+        freeTokenArray(tokens, tCount);
+    }
+    print("Goodbye!\n");
 }
 
 
 
 
 
+char** readT(int* tokenCount) {
+    print("> ");
+    char buffer[INPUT_LENGTH];
+    read(STDIN_FILENO, buffer, INPUT_LENGTH);
+    return readTokens(TOKEN_CNT + 1, TOKEN_LEN, tokenCount, buffer);
+    return NULL;
+}
 
 char** readTokens(int maxTokenNum, int maxTokenSize, int* readTokenNum, char* buffer)
 //Tokenize buffer
@@ -90,4 +146,31 @@ void freeTokenArray(char** tokenStrArr, int size) {
     //      afterwards
 }
 
+void print(char* str, ...) {
+    char buffer[INPUT_LENGTH];
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buffer, sizeof buffer, str, argptr);
+    va_end(argptr);
 
+    write(STDOUT_FILENO, buffer, strlen(buffer));
+}
+
+void printError(char * str, ...) {
+    char buffer[INPUT_LENGTH];
+    va_list argptr;
+    va_start(argptr, str);
+    vsnprintf(buffer, sizeof buffer, str, argptr);
+    va_end(argptr);
+
+    write(STDERR_FILENO, buffer, strlen(buffer));
+}
+
+int cfileexists(const char* filename) {
+    struct stat buffer;
+    int exist = stat(filename,&buffer);
+    if(exist == 0)
+        return 1;
+    else // -1
+        return 0;
+}
